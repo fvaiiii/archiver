c37ae178ed9a0200dfcc56cfa9cf0aa8eda1ec5d
daria <m2403815@edu.misis.ru>
2026-03-16 16:31:27 +0300

    implement reader and writer function

--- internal/archive/reader.go
+++ internal/archive/reader.go
@@ -1,1 +1,55 @@
 package archive
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/fvaiiii/archiver/internal/lz77"
+)
+
+func ReadArchive(path string) ([]lz77.Token, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка открытия архива: %w", err)
+	}
+	defer file.Close()
+
+	header := make([]byte, 4)
+	_, err = file.Read(header)
+	if err != nil {
+		return nil, err
+	}
+
+	if string(header) != "ARCV" {
+		return nil, fmt.Errorf("Неверный формат архива")
+	}
+
+	var tokenCount uint32
+	err = binary.Read(file, binary.LittleEndian, &tokenCount)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens := make([]lz77.Token, 0, tokenCount)
+	for i := uint32(0); i < tokenCount; i++ {
+		var token lz77.Token
+		err = binary.Read(file, binary.LittleEndian, &token.Offset)
+		if err != nil {
+			return nil, err
+		}
+
+		err = binary.Read(file, binary.LittleEndian, &token.Length)
+		if err != nil {
+			return nil, err
+		}
+
+		err = binary.Read(file, binary.LittleEndian, &token.NextByte)
+		if err != nil {
+			return nil, err
+		}
+
+		tokens = append(tokens, token)
+	}
+	return tokens, nil
+}
--- internal/archive/writer.go
+++ internal/archive/writer.go
@@ -1,1 +1,46 @@
 package archive
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/fvaiiii/archiver/internal/lz77"
+)
+
+func WriteArchive(path string, tokens []lz77.Token) error {
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("Ошибка открытия файла: %w", err)
+	}
+
+	defer file.Close()
+
+	_, err = file.Write([]byte("ARCV"))
+	if err != nil {
+		return err
+	}
+
+	err = binary.Write(file, binary.LittleEndian, uint32(len(tokens)))
+	if err != nil {
+		return fmt.Errorf("Ошибка записи количества токенов: %w", err)
+	}
+
+	for _, token := range tokens {
+		err = binary.Write(file, binary.LittleEndian, token.Offset)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(file, binary.LittleEndian, token.Length)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(file, binary.LittleEndian, token.NextByte)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
